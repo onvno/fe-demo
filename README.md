5a55f925ffc429a2d1b1879c9119a78597988f6e
onvno <leewei2020@126.com>
2018-06-16 00:09:51 +0800

    禁用文本选择

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 收集一些前端demo示例。
 
 * [zhihu.html](./zhihu.html):实现拷贝内容携带版权信息
+* [select](./select.js):禁用文本选择
---
+++ select.js
@@ -0,0 +1,5 @@
+// 页面禁用文本选择拷贝
+document.onselectstart=new Function("return false");
+
+// 页面开启文本选择拷贝
+document.onselectstart=new Function("return true");
